d56fc649f2c623fff24ff82cdcef98d11903a120
KostasVasilopoulos <k.vasilopoulo@gmail.com>
2018-07-12 01:39:46 +0300

    depends pipe operator %>% without adding to the Namespace

--- R/exuber-package.r
+++ R/exuber-package.r
@@ -7,3 +7,15 @@
 #' @keywords internal
 
 "_PACKAGE"
+
+#' Pipe operator
+#'
+#' See \code{\link[magrittr]{\%>\%}} for more details.
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL
