4887c5b5bae0e872f0002260dff86006b35a3a13
KostasVasilopoulos <k.vasilopoulo@gmail.com>
2019-02-04 01:46:03 +0000

    reduced size of the package, less than 5MB

--- data-raw/sim-crit.R
+++ data-raw/sim-crit.R
@@ -4,27 +4,15 @@
 
 # Simulation -------------------------------------------------------------
 
-nn <- 2000
+nn <- 700
 crit <- list()
 for (i in 6:nn) {
   crit[[i]] <- mc_cv(i)
+  print(i)
 }
 names(crit) <- c(paste0("_nan", 1:5), c(paste0("n", 6:nn)))
 class(crit) <- c("list", "crit")
 
-
-# change critical values badf_cv from simulated to fixed ------------------
-
-for (n in 6:nn) { #length(crit)
-  minw <- floor((0.01 + 1.8 / sqrt(n)) * n)
-  temp <- log(log(n*seq(minw + 1, n)))/100
-  new_cv <- matrix(rep(temp, 3), ncol = 3,
-                   dimnames = list(NULL, c("90%", "95%", "99%")))
-
-  crit[[n]]$"badf_cv" <- new_cv
-  attr(crit[[n]], "opt_badf") <- "fixed"
-}
-
 # ... ---------------------------------------------------------------------
 
-# usethis::use_data(crit, overwrite = TRUE)
+usethis::use_data(crit, overwrite = TRUE, compress = "xz")
