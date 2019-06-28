1669dc58352b272d0db00fe9c2c0cd86cbed1804
KostasVasilopoulos <k.vasilopoulo@gmail.com>
2019-06-28 12:39:05 +0100

    conform sim_dgp to sim_psy

--- R/deprecate.R
+++ R/deprecate.R
@@ -32,13 +32,18 @@ plot.radf <- function(x, y, option = c("gsadf", "sadf"), min_duration = 0,
 }
 
 #' @rdname exuber-deprecated
-sim_dgp1 <- function(...) {
+sim_dgp1 <- function(n, te = 0.4 * n, tf = 0.15 * n + te, c = 1,
+                     alpha = 0.6, sigma = 6.79, seed = NULL) {
   .Deprecated(new = "sim_psy1()", package = "exuber")
-  sim_psy1(...)
+  sim_psy1(n = n, te = te, tf = tf, c = c,
+           alpha = alpha, sigma = sigma, seed = seed)
 }
 
 #' @rdname exuber-deprecated
-sim_dgp2 <- function(...) {
+sim_dgp2 <- function(n, te1 = 0.2 * n, tf1 = 0.2 * n + te1,
+                     te2 = 0.6 * n, tf2 = 0.1 * n + te2,
+                     c = 1, alpha = 0.6, sigma = 6.79, seed = NULL) {
   .Deprecated("sim_psy2()", package = "exuber")
-  sim_psy2(...)
+  sim_psy2(n = n, te1 = te1, tf1 = tf1, te2 = te2, tf2 = tf2, c = c,
+           alpha = alpha, sigma = sigma, seed = seed)
 }
