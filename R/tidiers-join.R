686ce7cdf1a82a112488be337b07ea406eb83ce8
KostasVasilopoulos <k.vasilopoulo@gmail.com>
2020-04-17 00:15:13 +0100

    consistency in tidiers and messages

--- R/tidiers-join.R
+++ R/tidiers-join.R
@@ -1,9 +1,14 @@
 #' Tidy into a joint model
 #'
-#' Tidy and join or augment and join objects of class `radf` and `cv`.
+#' Tidy, agument or glance, and then join objects of class `radf` and `cv`. The
+#' object of reference should be the `radf`. For example, using `glance` in an
+#' radf object returns the panel statistic, so `glance_join` returns the panel
+#' statistic together with the critical values.
 #'
 #' @param x An object of class `radf`.
-#' @param y An object of class `cv`.
+#' @param y An object of class `cv`. The output will depend on the type of
+#' critical value.
+#'
 #'
 #' @importFrom dplyr full_join case_when
 #' @export
@@ -12,28 +17,21 @@ tidy_join <- function(x, y = NULL) {
   assert_class(x, "radf")
   y <- y %||% retrieve_crit(x)
   assert_class(y, "cv")
+  if (is_sb(y)) {
+    stop_glue(
+      "argument 'y' should not be of class 'sb_cv', ",
+      "do you need 'glance_join'")
+  }
   assert_match(x, y)
 
-
-  if (is_mc(y)) {
-    tbl <- inner_join(
-      tidy(x, format = "long"),
-      tidy(y, format = "long"),
-      by = "name")
-  }else if (is_wb(y)) {
-    tbl <- inner_join(
-      tidy(x, format = "long"),
-      tidy(y, format = "long"),
-      by = c("name", "id"))
-  }else if (is_sb(y)) {
-    tbl <- inner_join(
-      glance(x, format = "long"),
-      tidy(y, format = "long"),
-      by = c("id", "name"))
-  }
-  tbl %>%
+  join_by <- if (!is_mc(y)) c("id") else NULL
+  inner_join(
+    tidy(x, format = "long"),
+    tidy(y, format = "long"),
+    by = c("name", join_by)) %>%
     arrange(name)
 }
+
 
 #' @rdname tidy_join
 #' @importFrom dplyr inner_join select case_when
@@ -45,28 +43,27 @@ augment_join <- function(x, y = NULL) {
   assert_class(y, "cv")
   assert_match(x, y)
 
-  #TODO reformat this code to make it smaller
+  panel_arg <- is_sb(y)
+  join_by <- if (!is_mc(y)) c("index", "id") else NULL
 
-  if (is_mc(y)) {
-    tbl <- inner_join(
-      augment(x, "long"),
-      augment(y, "long"),
-      by = c("key", "name")
-    )
-  } else if (is_wb(y)) {
-    tbl <- inner_join(
-      augment(x, "long"),
-      augment(y, "long"),
-      by = c("key","index", "id", "name")
-    ) %>%
-      arrange(id, name, sig)
-  } else if (is_sb(y)) {
-    tbl <- inner_join(
-      augment(x, "long", panel = TRUE),
-      augment(y, "long"),
-      by = c("key", "index", "id", "name")
-    )
-  }
-  tbl %>%
+  inner_join(
+    augment(x, "long", panel = panel_arg),
+    augment(y, "long"), by = c("key", "name", join_by)
+  ) %>%
+    arrange(sig, id, name) %>%
     select(-key)
+}
+
+#' @rdname tidy_join
+#' @importFrom dplyr inner_join select case_when
+#' @export
+glance_join <- function(x, y) {
+  if (!is_sb(y)) {
+    stop_glue("argument 'y' should be of class 'sb_cv'")
+  }
+  inner_join(
+    glance(x, format = "long"),
+    tidy(y, format = "long"),
+    by = c("id", "name")) %>%
+    arrange(name)
 }
