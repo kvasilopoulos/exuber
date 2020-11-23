a5053668c508e2bb6dc21fc1596a388204842b1c
KostasVasilopoulos <k.vasilopoulo@gmail.com>
2020-11-23 17:57:52 +0000

    updated class in local data and exuberdata. Closes #12

--- data-raw/sim-crit.R
+++ data-raw/sim-crit.R
@@ -17,16 +17,17 @@ class(radf_crit) <- c("crit", "list")
 
 # test and store ----------------------------------------------------------
 
-is_equal <- all.equal(radf_crit[[100]], radf_mc_cv(100, nrep = 2000L, seed = 123))
+all_equal <- all.equal(radf_crit[[100]], radf_mc_cv(100, nrep = 2000, seed = 123))
 
 if (isTRUE(is_equal)) {
   usethis::use_data(radf_crit, overwrite = TRUE , compress = "xz")
 }
 
+waldo::compare(radf_crit[[100]], radf_mc_cv(100, nrep = 2000, seed = 123))
+waldo::compare(radf_crit[[100]], exuberdata::radf_crit2[[100]])
+
+# change ------------------------------------------------------------------
+
 for (i in 6:600) {
-  store_attrs <- attributes(radf_crit[[i]])
-  attributes(radf_crit[[i]]) <- NULL
-  attr(radf_crit[[i]], "index") <- 1:i
-  attributes(radf_crit[[i]]) <- c(attributes(radf_crit[[i]]), store_attrs)
-  class(radf_crit[[i]]) <- c("radf_cv", "mc_cv", "cv", "list")
+  class(radf_crit[[i]]) <- c("radf_cv", "mc_cv", "list")
 }
