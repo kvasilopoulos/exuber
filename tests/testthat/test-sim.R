43e28df992a3b03759e3427151a72cfe705ac717
KostasVasilopoulos <k.vasilopoulo@gmail.com>
2021-05-31 11:12:41 +0100

    more tests in sim

--- tests/testthat/test-sim.R
+++ tests/testthat/test-sim.R
@@ -1,6 +1,7 @@
 context("sim")
 
 test_that("sim_evans", {
+  expect_error(sim_evans(100), regexp = NA)
   expect_error(
     sim_evans(100, delta = 1.5),
     message("alpha and delta should satisfy: 0 < delta < (1+r)*alpha")
@@ -12,7 +13,7 @@ test_that("sim_div", {
   expect_error(sim_div(100, output = "d"), regexp = NA)
 })
 
-test_that("psy1", {
+test_that("sim_psy1", {
   expect_error(
     sim_psy1(100, te = 120),
     message("Argument 'te' should be a be between '0' and '100' ")
@@ -21,4 +22,31 @@ test_that("psy1", {
     sim_psy1(100, tf = 120),
     message("Argument 'tf' should be a be between '0' and '100' ")
   )
+})
+
+test_that("sim_psy2", {
+  expect_error(
+    sim_psy2(100, te1 = 120),
+    message("Argument 'te1' should be a be between '0' and '100' ")
+  )
+  expect_error(
+    sim_psy2(100, tf1 = 120),
+    message("Argument 'tf' should be a be between '0' and '100' ")
+  )
+  expect_error(
+    sim_psy2(100, alpha = 1.1),
+    message("Argument 'alpha' should be a be between '0' and '1' ")
+  )
+})
+
+test_that("sim_ps1",{
+  expect_error(
+    sim_ps1(100),
+    regexp = NA
+  )
+  expect_error(
+    autoplot(sim_ps1(100)),
+    regexp = NA
+  )
+
 })
